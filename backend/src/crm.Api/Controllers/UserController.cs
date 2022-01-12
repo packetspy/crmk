d5946c39eb9a8cc7f01c69b6e95c4806d36fbecd
Anderson Tomazeto <packetspy@gmail.com>
2022-01-12 00:06:58 -0300

    CRUD User

--- backend/src/crm.Api/Controllers/UserController.cs
+++ backend/src/crm.Api/Controllers/UserController.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using crm.Domain.Interface;
+using System.ComponentModel.DataAnnotations;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace crm.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/users")]
+    [Display(Description = "Usuários")]
     public class UserController : ControllerBase
     {
         private IUserService _userService;
@@ -21,9 +23,35 @@ namespace crm.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult GetUsers()
         {
-            return Ok("Rodando até aqui");
+            return Ok("Retornando lista de usuários");
+        }
+
+        [HttpGet]
+        [Route("{publicId:guid}")]
+        public IActionResult GetUser(Guid publicId)
+        {
+            return Ok("Retornando usuário único");
+        }
+
+        [HttpPost]
+        public IActionResult PostUser([FromBody] string Name)
+        {
+            return Ok($"Salvando usuário: {Name}");
+        }
+
+        [HttpPut]
+        public IActionResult PutUser([FromBody] string Name)
+        {
+            return Ok($"Atualizando usuário: {Name}");
+        }
+
+        [HttpDelete]
+        [Route("{publicId:guid}")]
+        public IActionResult DeleteUser(Guid publicId)
+        {
+            return Ok("Usuário deletado.");
         }
     }
 }
