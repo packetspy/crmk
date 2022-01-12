2f44660d9efd11e25bd1a03a5decebfeebd21d46
Anderson Tomazeto <packetspy@gmail.com>
2022-01-12 00:06:00 -0300

    Add first Model

---
+++ backend/src/crm.Domain/Model/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.Domain.Model
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public Guid PublicId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
---
+++ backend/src/crm.Domain/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.Domain.Model
+{
+    public class User : Entity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
